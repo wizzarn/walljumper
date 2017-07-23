e83f1f11a8335ddcd6d17acd5523cb0f9cedcdc6
Jose Garcia <wizzarn@gmail.com>
2017-07-23 17:13:34 -0500

    bird movement, and police movement

---
+++ Assets/Scripts/VectorFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorFollow : MonoBehaviour {
+
+	public Transform mTarget;
+	Vector3 mLookAtDirection;
+	float mSpeed = .8f;
+	const float EPSILON = .1f;
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		mLookAtDirection = (mTarget.position - transform.position).normalized;
+		if ((transform.position - mTarget.position).magnitude > EPSILON)
+			transform.Translate (mLookAtDirection * Time.deltaTime * mSpeed);
+	}
+}
