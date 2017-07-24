601257908dc92992148d212c81dcbe37ebb4a6de
Victor Daniel Gonzalez Flores <victor.gzz.flores@hotmail.com>
2017-07-23 21:18:26 -0500

    prefab for wall hole

---
+++ Assets/Scripts/Obstacles/AutoDestroy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour {
+	public float time;
+	private IEnumerator Start()
+	{
+		 yield return new WaitForSeconds( time );
+		 Destroy( gameObject ); 
+	}
+}
